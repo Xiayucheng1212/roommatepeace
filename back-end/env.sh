3c5ac845fbfc93a3153ce35f617f7ec2245f4952
Xia <u3566749@connect.hku.hk>
2021-06-04 22:17:09 +0800

    chenge

--- back-end/env.sh
+++ back-end/env.sh
@@ -4,6 +4,6 @@ export NODE_ENV=development
 export PG_USERNAME=postgres
 export PG_HOSTNAME=localhost
 export PG_PORT=5432
-export PG_DB_NAME=weathermood
+export PG_DB_NAME=roommatepeace
 
 echo "DONE"
